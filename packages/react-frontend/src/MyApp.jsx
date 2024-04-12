c2e84e1ec048d3681d063acd84bf121bac581093
JosueHde3z <josue.hernandez20@aol.com>
2024-04-12 13:59:19 -0700

    IE1_MyFirstReactApp

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,103 @@
+// // src/MyApp.jsx
+// import React from "react";
+
+// function MyApp() {
+//   return (
+//     <div>
+//       <h1>Hello, React!</h1>
+//     </div>
+//   );
+// }
+// export default MyApp;
+///////////////////////////////////////////////////////////////////////////////
+// src/MyApp.jsx
+// import React from "react";
+// import Table from "./Table";
+
+// function MyApp() {
+//   return (
+//     <div className="container">
+//       <Table />
+//     </div>
+//   );
+// }
+///////////////////////////////////////////////////////////////////////////////
+// src/MyApp.jsx
+// import React from "react";
+// import Table from "./Table";
+
+// const characters = [
+//     {
+//       name: "Charlie",
+//       job: "Janitor"
+//     },
+//     {
+//       name: "Mac",
+//       job: "Bouncer"
+//     },
+//     {
+//       name: "Dee",
+//       job: "Aspring actress"
+//     },
+//     {
+//       name: "Dennis",
+//       job: "Bartender"
+//     }
+//   ];
+  
+//   function MyApp() {
+//     return (
+//         <div className="container">
+//             <Table characterData={characters} />
+//         </div>
+//         );
+//     }
+// export default MyApp;
+///////////////////////////////////////////////////////////////////////////////
+// import React, { useState } from "react";
+// import Table from "./Table";
+
+// function MyApp() {
+//   const [characters, setCharacters] = useState([
+//     {
+//       name: "Charlie",
+//       job: "Janitor"
+//     } // the rest of the data
+//   ]);
+
+//   return (
+//     <div className="container">
+//       <Table characterData={characters} />
+//     </div>
+//   );
+// }
+// export default MyApp;
+///////////////////////////////////////////////////////////////////////////////
+import React, { useState } from "react";
+import Table from "./Table";
+
+function MyApp() {
+  const [characters, setCharacters] = useState([
+    {
+      name: "Charlie",
+      job: "Janitor" // the rest of the data
+    }
+  ]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+    
+  }
+  return (
+    <div className="container">
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
+    </div>
+  );
+}
+export default MyApp;
---
+++ packages/react-frontend/src/Table.jsx
@@ -0,0 +1,225 @@
+// // src/Table.jsx
+// import React from "react";
+
+// function Table() {
+//   return (
+//     <table>
+//       <thead>
+//         <tr>
+//           <th>Name</th>
+//           <th>Job</th>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         <tr>
+//           <td>Charlie</td>
+//           <td>Janitor</td>
+//         </tr>
+//         <tr>
+//           <td>Mac</td>
+//           <td>Bouncer</td>
+//         </tr>
+//         <tr>
+//           <td>Dee</td>
+//           <td>Aspiring actress</td>
+//         </tr>
+//         <tr>
+//           <td>Dennis</td>
+//           <td>Bartender</td>
+//         </tr>
+//       </tbody>
+//     </table>
+//   );
+// }
+
+// export default Table;
+
+// src/Table.jsx
+// import React from "react";
+
+// function TableHeader() {
+//     return (
+//       <thead>
+//         <tr>
+//           <th>Name</th>
+//           <th>Job</th>
+//         </tr>
+//       </thead>
+//     );
+//   }
+  
+// function TableBody() {
+// return <tbody />;
+// }
+
+// function Table() {
+//     return (
+//       <table>
+//         <TableHeader />
+//         <TableBody />
+//       </table>
+//     );
+// }
+
+// export default Table;
+
+// // src/Table.jsx
+// import React from "react";
+
+// function TableHeader() {
+//     return (
+//       <thead>
+//         <tr>
+//           <th>Name</th>
+//           <th>Job</th>
+//         </tr>
+//       </thead>
+//     );
+//   }
+  
+// function TableBody() {
+// return <tbody />;
+// }
+
+// function Table(props) {
+//     return (
+//       <table>
+//         <TableHeader />
+//         <TableBody characterData={props.characterData} />
+//       </table>
+//     );
+// }
+
+// export default Table;
+
+// src/Table.jsx
+// import React from "react";
+
+// function TableHeader() {
+//     return (
+//       <thead>
+//         <tr>
+//           <th>Name</th>
+//           <th>Job</th>
+//         </tr>
+//       </thead>
+//     );
+//   }
+  
+// function TableBody(props) {
+// const rows = props.characterData.map((row, index) => {
+//     return (
+//     <tr key={index}>
+//         <td>{row.name}</td>
+//         <td>{row.job}</td>
+//     </tr>
+//     );
+//     }
+// );
+// return (
+//     <tbody>
+//         {rows}
+//         </tbody>
+//     );
+// }
+
+// function Table(props) {
+//     return (
+//     <table>
+//         <TableHeader />
+//         <TableBody characterData={props.characterData} />
+//     </table>
+//     );
+// }
+// export default Table;
+///////////////////////////////////////////////////////////////////////////////
+// import React from "react";
+
+// function TableHeader() {
+//     return (
+//       <thead>
+//         <tr>
+//           <th>Name</th>
+//           <th>Job</th>
+//         </tr>
+//       </thead>
+//     );
+//   }
+  
+// function TableBody(props) {
+// const rows = props.characterData.map((row, index) => {
+//     return (
+//     <tr key={index}>
+//         <td>{row.name}</td>
+//         <td>{row.job}</td>
+//     </tr>
+//     );
+//     }
+// );
+// return (
+//     <tbody>
+//         {rows}
+//         </tbody>
+//     );
+// }
+
+// function Table(props) {
+//     return (
+//       <table>
+//         <TableHeader />
+//         <TableBody
+//           characterData={props.characterData}
+//           removeCharacter={props.removeCharacter}
+//         />
+//       </table>
+//     );
+//   }
+// export default Table;
+///////////////////////////////////////////////////////////////////////////////
+import React from "react";
+
+function TableHeader() {
+    return (
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Job</th>
+        </tr>
+      </thead>
+    );
+  }
+  
+function TableBody(props) {
+const rows = props.characterData.map((row, index) => {
+    return (
+        <tr key={index}>
+        <td>{row.name}</td>
+        <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>
+            Delete
+          </button>
+        </td>
+      </tr>
+    );
+    }
+);
+return (
+    <tbody>
+        {rows}
+        </tbody>
+    );
+}
+
+function Table(props) {
+    return (
+      <table>
+        <TableHeader />
+        <TableBody
+          characterData={props.characterData}
+          removeCharacter={props.removeCharacter}
+        />
+      </table>
+    );
+  }
+export default Table;
