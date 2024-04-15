9ae7bf23e73f814194dbecb3adb1e50a4d05f9eb
JosueHde3z <josue.hernandez20@aol.com>
2024-04-15 14:01:56 -0700

    IE2 Step 4

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -16,6 +16,54 @@
 //   );
 // });
 // /////////////////////////////////////////////////////////
+// // backend.js
+// import express from "express";
+
+// const app = express();
+// const port = 8000;
+// const users = {
+//     users_list: [
+//       {
+//         id: "xyz789",
+//         name: "Charlie",
+//         job: "Janitor"
+//       },
+//       {
+//         id: "abc123",
+//         name: "Mac",
+//         job: "Bouncer"
+//       },
+//       {
+//         id: "ppp222",
+//         name: "Mac",
+//         job: "Professor"
+//       },
+//       {
+//         id: "yat999",
+//         name: "Dee",
+//         job: "Aspring actress"
+//       },
+//       {
+//         id: "zap555",
+//         name: "Dennis",
+//         job: "Bartender"
+//       }
+//     ]
+//   };
+// app.use(express.json());
+
+// // app.get("/", (req, res) => {
+// //   res.send("Hello World!");
+// // });
+// app.get("/users", (req, res) => {
+//     res.send(users);
+//   });
+// app.listen(port, () => {
+//   console.log(
+//     `Example app listening at http://localhost:${port}/users"`
+//   );
+// });
+// /////////////////////////////////////////////////////////
 // backend.js
 import express from "express";
 
@@ -50,14 +98,25 @@ const users = {
       }
     ]
   };
-app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   res.send("Hello World!");
-// });
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+        (user) => user["name"] === name
+    );
+};
+app.use(express.json());
+  
 app.get("/users", (req, res) => {
+const name = req.query.name;
+if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+} else {
     res.send(users);
-  });
+}
+});
+
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}/users"`
