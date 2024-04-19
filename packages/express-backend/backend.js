b79255efabdab3f66dba2735b1dcd013866778a2
JosueHde3z <josue.hernandez20@aol.com>
2024-04-19 13:54:31 -0700

    IE3 Part 5

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -185,90 +185,303 @@
 //   );
 // });
 // /////////////////////////////////////////////////////////
+// // backend.js
+// import express from "express";
+// import cors from "cors";
+
+// const app = express();
+// const port = 8000;
+// const users = {
+//     users_list: [
+//       {
+//         id: "xyz789",
+//         name: "Charlie",
+//         job: "Janitor"
+//       },
+//       {
+//         id: "abc123",
+//         name: "Mac",
+//         job: "Bouncer"
+//       },
+//       {
+//         id: "ppp222",
+//         name: "Mac",
+//         job: "Professor"
+//       },
+//       {
+//         id: "yat999",
+//         name: "Dee",
+//         job: "Aspring actress"
+//       },
+//       {
+//         id: "zap555",
+//         name: "Dennis",
+//         job: "Bartender"
+//       }
+//     ]
+//   };
+
+// const findUserByName = (name) => {
+//     return users["users_list"].filter(
+//         (user) => user["name"] === name
+//     );
+// };
+  
+// const findUserById = (id) =>
+//   users["users_list"].find((user) => user["id"] === id);
+
+// const addUser = (user) => {
+//     users["users_list"].push(user);
+//     return user;
+// };
+
+// const delUser = (id) => {
+//   // Find the user index by ID
+//   const index = users["users_list"].findIndex((user) => user.id === id);
+  
+//   // If the user exists, remove it from the array
+//   if (index !== -1) {
+//     const deletedUser = users["users_list"].splice(index, 1)[0]; // Remove the user and get the deleted user
+//     return deletedUser;
+//   } else {
+//     return null; // User not found
+//   }
+// };
+
+// //Adding search all for matching job/name
+// const findUsersByNameAndJob = (name, job) => {
+//   return users["users_list"].filter(
+//     (user) => user.name === name && user.job === job
+//   );
+// }
+
+// app.post("/users", (req, res) => {
+//     const userToAdd = req.body;
+//     addUser(userToAdd);
+//     res.status(201).send(); //setting the response status code to 201
+// });
+
+// app.use(cors());
+
+// app.use(express.json());
+// //Delete function
+// app.delete("/users/:id", (req, res) => {
+//   const id = req.params.id;
+//   const deletedUser = delUser(id);
+//   if (deletedUser !== null) {
+//     res.send(`User with ID ${id} has been deleted.`);
+//   } else {
+//     res.status(404).send("User not found.");
+//   }
+// });
+
+// app.get("/", (req, res) => {
+//     res.send("Hello World!");
+//   });
+
+// // app.get("/users", (req, res) => {
+// //     const name = req.query.name;
+// //     if (name != undefined) {
+// //         let result = findUserByName(name);
+// //         result = { users_list: result };
+// //         res.send(result);
+// //     } else {
+// //         res.send(users);
+// //     }
+// // });
+
+// app.get("/users", (req, res) => {
+//   const name = req.query.name;
+//   const job = req.query.job;
+
+//   if (name !== undefined && job !== undefined) {
+//     let result = findUsersByNameAndJob(name, job);
+//     result = { users_list: result };
+//     res.send(result);
+//   } else if (name !== undefined) {
+//     let result = findUserByName(name);
+//     result = { users_list: result };
+//     res.send(result);
+//   } else {
+//     res.send(users);
+//   }
+// });
+
+// app.get("/users/:id", (req, res) => {
+//   const id = req.params["id"]; //or req.params.id
+//   let result = findUserById(id);
+//   if (result === undefined) {
+//     res.status(404).send("Resource not found.");
+//   } else {
+//     res.send(result);
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(
+//     `Example app listening at http://localhost:${port}"`
+//   );
+// });
+// /////////////////////////////////////////////////////////
 // backend.js
 import express from "express";
 import cors from "cors";
 
 const app = express();
-const port = 8000;
-const users = {
-    users_list: [
-      {
-        id: "xyz789",
-        name: "Charlie",
-        job: "Janitor"
-      },
-      {
-        id: "abc123",
-        name: "Mac",
-        job: "Bouncer"
-      },
-      {
-        id: "ppp222",
-        name: "Mac",
-        job: "Professor"
-      },
-      {
-        id: "yat999",
-        name: "Dee",
-        job: "Aspring actress"
-      },
-      {
-        id: "zap555",
-        name: "Dennis",
-        job: "Bartender"
-      }
-    ]
-  };
-
-const findUserByName = (name) => {
-    return users["users_list"].filter(
-        (user) => user["name"] === name
-    );
-};
-  
-const findUserById = (id) =>
-  users["users_list"].find((user) => user["id"] === id);
-
-const addUser = (user) => {
-    users["users_list"].push(user);
-    return user;
-};
-
-app.post("/users", (req, res) => {
-    const userToAdd = req.body;
-    addUser(userToAdd);
-    res.send();
-});
 
 app.use(cors());
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-        let result = findUserByName(name);
-        result = { users_list: result };
-        res.send(result);
-    } else {
-        res.send(users);
-    }
+const port = 8000;
+const users = {
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
+    },
+    {
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
+    },
+    {
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
+    },
+    {
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
+
+const findUserByName = (name) => 
+{
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+const findUserById = (id) => 
+{
+  users["users_list"].find((user) => user["id"] === id);
+};
+
+const addUser = (user) => 
+{
+  const newUser = 
+  {
+    id: generateId(), // Generate random ID
+    ...user,
+  };
+  users["users_list"].push(newUser);
+  return newUser;
+};
+
+const delUser = (id) => 
+{
+  // Find the user index by ID
+  const index = users["users_list"].findIndex((user) => user.id === id);
+
+  // If the user exists, remove it from the array
+  if (index !== -1) //-1 means no user found so if it find something delete it
+  {
+    const deletedUser = users["users_list"].splice(index, 1)[0]; // Remove the user and get the deleted user
+    // const deletedUser = users["users_list"].splice(index, 1); // Remove the user and get the deleted user
+    return deletedUser;
+  } 
+  else 
+  {
+    return null; // User not found
+  }
+};
+
+// ID generator function
+const generateId = () => 
+{
+  return Math.random().toString(36).substr(2, 9); // Generate random alphanumeric ID
+};
+
+// Adding search all for matching job/name
+const findUsersByNameAndJob = (name, job) => 
+{
+  return users["users_list"].filter((user) => user.name === name && user.job === job);
+};
+
+app.post("/users", (req, res) => 
+{
+  const userToAdd = req.body;
+  const newUser = addUser(userToAdd);
+  res.status(201).send(newUser); // Setting the response status code to 201 and sending the newly created user
 });
 
-app.get("/users/:id", (req, res) => {
+
+
+app.delete("/users/:id", (req, res) => 
+{
+  const id = req.params.id;
+  const deletedUser = delUser(id);
+  if (deletedUser !== null) 
+  {
+    res.status(204).send(`User with ID ${id} has been deleted.`);
+  } 
+  else 
+  {
+    res.status(404).send("User not found.");
+  }
+});
+
+app.get("/", (req, res) => 
+{
+  res.send("Hello World!");
+});
+
+app.get("/users", (req, res) => 
+{
+  const name = req.query.name;
+  const job = req.query.job;
+
+  if (name !== undefined && job !== undefined) 
+  {
+    let result = findUsersByNameAndJob(name, job);
+    result = { users_list: result };
+    res.send(result);
+  } 
+  else if (name !== undefined) 
+  {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } 
+  else 
+  {
+    res.send(users);
+  }
+});
+
+app.get("/users/:id", (req, res) => 
+{
   const id = req.params["id"]; //or req.params.id
   let result = findUserById(id);
-  if (result === undefined) {
+  if (result === undefined) 
+  {
     res.status(404).send("Resource not found.");
-  } else {
+  } 
+  else 
+  {
     res.send(result);
   }
 });
 
-app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}"`
-  );
+app.listen(port, () => 
+{
+  console.log(`Example app listening at http://localhost:${port}`);
 });
 /////////////////////////////////////////////////////////
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -180,37 +180,368 @@
 //   );
 // }
 // export default MyApp;
+// ///////////////////////////////////////////////////////////////////////////////
+// // import React, { useState } from "react";
+// import React, {useState, useEffect} from 'react';
+// import Table from "./Table";
+// import Form from "./Form";
+
+// function MyApp() {
+//   const [characters, setCharacters] = useState([]);
+
+//   function removeOneCharacter(index) {
+//     const updated = characters.filter((character, i) => {
+//       return i !== index;
+//     });
+//     setCharacters(updated);
+    
+//   }
+//   function updateList(person) {
+//     setCharacters([...characters, person]);
+//   }
+
+//   function fetchUsers() {
+//     const promise = fetch("http://localhost:8000/users");
+//     return promise;
+//   }
+  
+//   useEffect(() => {
+//     fetchUsers()
+//       .then((res) => res.json())
+//       .then((json) => setCharacters(json["users_list"]))
+//       .catch((error) => { console.log(error); });
+//   }, [] );
+
+//   return (
+//     <div className="container">
+//       <Table
+//         characterData={characters}
+//         removeCharacter={removeOneCharacter}
+//       />
+//       <Form handleSubmit={updateList} />
+//     </div>
+//   );
+// }
+// export default MyApp;
+// ///////////////////////////////////////////////////////////////////////////////
+// // import React, { useState } from "react";
+// import React, { useState, useEffect } from 'react';
+// import Table from "./Table";
+// import Form from "./Form";
+
+// function MyApp() {
+//   const [characters, setCharacters] = useState([]);
+
+//   function removeOneCharacter(index) {
+//     const updated = characters.filter((character, i) => i !== index);
+//     setCharacters(updated);
+//   }
+
+//   function updateList(person) {
+//     postUser(person)
+//       .then((response) => {
+//         if (response.status === 201) {
+//           // User created successfully, update the state
+//           return response.json();
+//         } else {
+//           throw new Error('Failed to create user');
+//         }
+//       })
+//       .then((json) => setCharacters([...characters, json]))
+//       .catch((error) => {
+//         console.error('Error:', error);
+//       });
+//   }
+
+//   function fetchUsers() {
+//     return fetch("http://localhost:8000/users")
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error('Failed to fetch users');
+//         }
+//         return response.json();
+//       });
+//   }
+
+//   function postUser(person) {
+//     return fetch("http://localhost:8000/users", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(person),
+//     });
+//   }
+
+//   useEffect(() => {
+//     fetchUsers()
+//       .then((json) => setCharacters(json["users_list"]))
+//       .catch((error) => {
+//         console.error('Error:', error);
+//       });
+//   }, []);
+
+//   return (
+//     <div className="container">
+//       <Table
+//         characterData={characters}
+//         removeCharacter={removeOneCharacter}
+//       />
+//       <Form handleSubmit={updateList} />
+//     </div>
+//   );
+// }
+
+// export default MyApp;
+// ///////////////////////////////////////////////////////////////////////////////
+// // import React, { useState } from "react";
+// import React, { useState, useEffect } from 'react';
+// import Table from "./Table";
+// import Form from "./Form";
+
+// function MyApp() {
+//   const [characters, setCharacters] = useState([]);
+
+//   function removeOneCharacter(index) {
+//     const updated = characters.filter((character, i) => i !== index);
+//     setCharacters(updated);
+//   }
+
+//   function updateList(person) {
+//     postUser(person)
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error('Failed to create user');
+//         }
+//         return response.json();
+//       })
+//       .then((json) => {
+//         // Attach the newly added JSON object to the state
+//         setCharacters([...characters, json]);
+//       })
+//       .catch((error) => {
+//         console.error('Error:', error);
+//       });
+//   }
+
+//   function fetchUsers() {
+//     return fetch("http://localhost:8000/users")
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error('Failed to fetch users');
+//         }
+//         return response.json();
+//       });
+//   }
+
+//   function postUser(person) {
+//     return fetch("http://localhost:8000/users", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(person),
+//     });
+//   }
+
+//   useEffect(() => {
+//     fetchUsers()
+//       .then((json) => setCharacters(json["users_list"]))
+//       .catch((error) => {
+//         console.error('Error:', error);
+//       });
+//   }, []);
+
+//   return (
+//     <div className="container">
+//       <Table
+//         characterData={characters}
+//         removeCharacter={removeOneCharacter}
+//       />
+//       <Form handleSubmit={updateList} />
+//     </div>
+//   );
+// }
+
+// export default MyApp;
+// ///////////////////////////////////////////////////////////////////////////////
+// // import React, { useState } from "react";
+// import React, { useState, useEffect } from 'react';
+// import Table from "./Table";
+// import Form from "./Form";
+
+// function MyApp() {
+//   const [characters, setCharacters] = useState([]);
+
+//   // function removeOneCharacter(index) {
+//   //   const updated = characters.filter((character, i) => i !== index);
+//   //   setCharacters(updated);
+//   // }
+
+//   function removeOneCharacter(id) {
+//     // Make an HTTP DELETE request to the server
+//     fetch(`http://localhost:8000/users/${id}`, {
+//       method: 'DELETE'
+//     })
+//     .then((response) => {
+//       if (!response.ok) {
+//         throw new Error('Failed to delete user');
+//       }
+//       // Filter out the deleted character from the state
+//       const updated = characters.filter((character) => character.id !== id);
+//       setCharacters(updated);
+//     })
+//     .catch((error) => {
+//       console.error('Error:', error);
+//     });
+//   }
+
+//   function updateList(person) {
+//     postUser(person)
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error('Failed to create user');
+//         }
+//         return response.json();
+//       })
+//       .then((json) => {
+//         // Attach the newly added JSON object to the state
+//         setCharacters([...characters, json]);
+//       })
+//       .catch((error) => {
+//         console.error('Error:', error);
+//       });
+//   }
+
+//   function fetchUsers() {
+//     return fetch("http://localhost:8000/users")
+//       .then((response) => {
+//         if (!response.ok) {
+//           throw new Error('Failed to fetch users');
+//         }
+//         return response.json();
+//       });
+//   }
+
+//   function postUser(person) {
+//     return fetch("http://localhost:8000/users", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(person),
+//     });
+//   }
+
+//   useEffect(() => {
+//     fetchUsers()
+//       .then((json) => setCharacters(json["users_list"]))
+//       .catch((error) => {
+//         console.error('Error:', error);
+//       });
+//   }, []);
+
+//   return (
+//     <div className="container">
+//       <Table
+//         characterData={characters}
+//         removeCharacter={removeOneCharacter}
+//       />
+//       <Form handleSubmit={updateList} />
+//     </div>
+//   );
+// }
+
+// export default MyApp;
 ///////////////////////////////////////////////////////////////////////////////
 // import React, { useState } from "react";
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter(index) {
-    const updated = characters.filter((character, i) => {
-      return i !== index;
-    });
-    setCharacters(updated);
-    
-  }
-  function updateList(person) {
-    setCharacters([...characters, person]);
+  function removeOneCharacter(id) 
+  {
+    // Make an HTTP DELETE request to the server
+    fetch(`http://localhost:8000/users/${id}`, {method: 'DELETE'})
+      .then((response) => 
+        {
+          if (response.status === 204) 
+          {
+            // Delete operation successful, filter out the deleted character from the state
+            const updated = characters.filter((character) => character.id !== id);
+            setCharacters(updated);
+          } 
+          else if (response.status === 404) 
+          {
+            console.log('User not found.');
+          } 
+          else 
+          {
+            throw new Error('Failed to delete user');
+          }
+        }
+      )
+      .catch((error) => {
+        console.error('Error:', error);
+      });
   }
 
-  function fetchUsers() {
-    const promise = fetch("http://localhost:8000/users");
-    return promise;
+  function updateList(person) 
+  {
+    postUser(person)
+      .then((response) => 
+      {
+        if (!response.ok) 
+        {
+          throw new Error('Failed to create user');
+        }
+        return response.json();
+      })
+      .then((json) => 
+      {
+        // Attach the newly added JSON object to the charater
+        setCharacters([...characters, json]);
+      })
+      .catch((error) => 
+      {
+        console.error('Error:', error);
+      });
   }
-  
+
+  function fetchUsers() 
+  {
+    return fetch("http://localhost:8000/users")
+      .then((response) => 
+      {
+        if (!response.ok) 
+        {
+          throw new Error('Failed to fetch users');
+        }
+        return response.json();
+      });
+  }
+
+  function postUser(person) 
+  {
+    return fetch("http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(person),
+    });
+  }
+
   useEffect(() => {
     fetchUsers()
-      .then((res) => res.json())
       .then((json) => setCharacters(json["users_list"]))
-      .catch((error) => { console.log(error); });
-  }, [] );
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
 
   return (
     <div className="container">
@@ -222,4 +553,5 @@ function MyApp() {
     </div>
   );
 }
+
 export default MyApp;
